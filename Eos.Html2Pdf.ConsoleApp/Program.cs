7dc6aafc12d1df572716220fa0909923c2d07fac
FakhrutdinovTN <FakhrutdinovTN@DESKTOP-TSHT6UC>
2020-08-04 12:26:32 +0500

    *

---
+++ Eos.Html2Pdf.ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Eos.Html2Pdf.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var files = new[]
+                {
+                    new FileInfo(@"sources\ответ_0.html"),
+                    new FileInfo(@"sources\ответ_1.html"),
+                    new FileInfo(@"sources\ответ_43.html")
+                };
+                var targetDir = new DirectoryInfo("targets");
+                if (!targetDir.Exists)
+                    Directory.CreateDirectory(targetDir.FullName);
+
+                foreach (var file in files)
+                {
+                    if (!file.Exists)
+                    {
+                        Console.WriteLine($"File {file.FullName} not exist");
+                        continue;
+                    }
+                    var bytes = File.ReadAllBytes(file.FullName);
+                    File.WriteAllBytes(Path.Combine(targetDir.FullName, $"{file.Name}_{Guid.NewGuid()}.pdf"), Eos.Html2Pdf.Html2PdfHelper.Convert(bytes));
+                }
+
+                Console.WriteLine("done!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Eos.Html2Pdf/Html2PdfHelper.cs
@@ -0,0 +1,79 @@
+﻿using PuppeteerSharp;
+using PuppeteerSharp.Media;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Eos.Html2Pdf
+{
+    public static class Html2PdfHelper
+    {
+        private const string default_chrome_path = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+
+        public static byte[] Convert(byte[] httpSource, string executablePath = default_chrome_path)
+        {
+            return Convert(ConvertAsync(httpSource, executablePath));
+        }
+
+        public static byte[] Convert(string httpPath, string executablePath = default_chrome_path)
+        {
+            return Convert(ConvertAsync(httpPath, executablePath));
+        }
+
+        private static byte[] Convert(Task<byte[]> task)
+        {
+            if (!task.Wait(TimeSpan.FromSeconds(60)) || task.Exception != null)
+                throw new Exception($"В процессе конвертирования документа http в pdf произошла ошибка. {task.Exception.Message}", task.Exception);
+            return task.Result;
+        }
+
+        public static async Task<byte[]> ConvertAsync(byte[] httpSource, string executablePath = default_chrome_path)
+        {
+            var htmlPath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.html");
+            try
+            {
+                File.WriteAllBytes(htmlPath, httpSource);
+                return await ConvertAsync(htmlPath, executablePath).ConfigureAwait(false);
+            }
+            finally
+            {
+                DeleteFile(htmlPath);
+            }
+        }
+
+        public static async Task<byte[]> ConvertAsync(string httpPath, string executablePath = default_chrome_path)
+        {
+            using (var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                ExecutablePath = executablePath,
+                Headless = true
+            }).ConfigureAwait(false))
+            {
+                var page = await browser.NewPageAsync();
+
+                await page.GoToAsync(httpPath).ConfigureAwait(false);
+
+                var pdfOptions = new PdfOptions
+                {
+                    Format = PaperFormat.A4,
+                    DisplayHeaderFooter = false
+                };
+
+                return await page.PdfDataAsync(pdfOptions).ConfigureAwait(false);
+            }
+        }
+
+        private static void DeleteFile(string fileName)
+        {
+            var fileInfo = new FileInfo(fileName);
+            if (!fileInfo.Exists)
+                return;
+            try
+            {
+                fileInfo.Attributes = FileAttributes.Normal;
+                fileInfo.Delete();
+            }
+            catch { }
+        }
+    }
+}
